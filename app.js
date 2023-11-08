ec5ac5a337cea2d9b9faf47e4faf6770547833d5
Alex Meiburg <timeroot.alex@gmail.com>
2023-11-08 15:26:56 -0500

    Works with inequality and theorem tracking. A0PP, AWPP, IP, LOGCFL, MIP, MIP*, MIPco, NEXP, QIP, SAC, SBP.

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ var path = require('path');
 var app = express();
 
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 /*app.get('/data', function(req, res){
     var data = require('./data.json');
     res.json(data);
 });*/
-app.use(express.static('public'));
+app.use(express.static('.'));
 app.listen(3000, function() {
     console.log('Server running on port 3000!');
 });
